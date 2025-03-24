ff12ea6d75c64a90a3711f0f872d8a353f3af388
lukasz <grzegorzewski.luk@gmail.com>
2025-03-24 21:56:54 +0100

    add: sanity : live - click-to-edit feature

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,17 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true,
+  //  click-to-edit features in Sanity
+  stega: {
+    studioUrl: process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/studio`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+  },
+});
