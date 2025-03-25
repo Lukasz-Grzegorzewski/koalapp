d6bed8c014cc7f480d97df41fe63c20f7e0ed746
lukasz <grzegorzewski.luk@gmail.com>
2025-03-25 15:17:50 +0100

    SearchPage for products matching query

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,10 +1,38 @@
+import ProductsGrid from "@/components/ProductsGrid";
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
+
 export default async function SearchPage({
   searchParams,
 }: {
   searchParams: { query: string };
 }) {
   const { query } = await searchParams;
-  console.log(`query => `, query);
 
-  return <div>Search {query}</div>;
+  const products = await searchProductsByName(query);
+
+  if (!products.length) {
+    return (
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+          <h1 className="text-3xl font-bold mb-6 text-center">
+            No products found for: {query}
+          </h1>
+          <p className="text-gray-600 text-center">
+            Try searching with different keywords
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="p-8 bg-white rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search result for {query}
+        </h1>
+        <ProductsGrid products={products} />
+      </div>
+    </div>
+  );
 }
