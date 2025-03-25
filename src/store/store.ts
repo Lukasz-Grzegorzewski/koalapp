fbafca7f0b0f8e3d6bbc02fff946f4d1e52f028a
lukasz <grzegorzewski.luk@gmail.com>
2025-03-26 00:11:00 +0100

    add zustand module & basketStore

---
+++ src/store/store.ts
@@ -0,0 +1,69 @@
+import { Product } from "../../sanity.types";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+export type BasketItemType = {
+  product: Product;
+  quantity: number;
+};
+
+type BasketState = {
+  items: BasketItemType[];
+  addItem: (product: Product) => void;
+  removeItem: (productId: string) => void;
+  clearBasket: () => void;
+  getTotalPrice: () => number;
+  getItemCount: (productId: string) => number;
+  getGroupedItems: () => BasketItemType[];
+};
+
+const useBasketStore = create<BasketState>()(
+  persist(
+    (set, get) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => {
+          const existingItem = state.items.find(
+            (item) => item.product._id === product._id
+          );
+          if (existingItem) {
+            return {
+              items: state.items.map((item) =>
+                item.product._id === product._id
+                  ? { ...item, quantity: item.quantity + 1 }
+                  : item
+              ),
+            };
+          } else {
+            return { items: [...state.items, { product, quantity: 1 }] };
+          }
+        }),
+      removeItem: (productId: string) =>
+        set((state) => ({
+          items: state.items.map((item: BasketItemType) => {
+            if (item.product._id === productId && item.quantity > 1)
+              return { ...item, quantity: item.quantity - 1 };
+            return item;
+          }),
+        })),
+      clearBasket: () => set({ items: [] }),
+      getTotalPrice: () => {
+        return get().items.reduce(
+          (total, item) => total + (item.product.price ?? 0) * item.quantity,
+          0
+        );
+      },
+      getItemCount: (productId: string) => {
+        const item = get().items.find((i) => i.product._id === productId);
+        return item ? item.quantity : 0;
+      },
+      getGroupedItems: () => get().items,
+    }),
+    {
+      name: "basket-store",
+      storage: createJSONStorage(() => sessionStorage), // not necessary because it is a default anyway
+    }
+  )
+);
+
+export default useBasketStore;
